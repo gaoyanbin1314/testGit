a54aadb6b64819b04c1ea598f95cf04ce4d8c8a6
gaoyanbin <yanbin@yxcfu.com>
2016-12-15 13:57:41 +0800

    提交新分支

---
+++ gitTest/gitTest/secondViewController.m
@@ -0,0 +1,37 @@
+//
+//  secondViewController.m
+//  gitTest
+//
+//  Created by 高艳彬 on 2016/12/15.
+//  Copyright © 2016年 YBKit. All rights reserved.
+//
+
+#import "secondViewController.h"
+
+@interface secondViewController ()
+
+@end
+
+@implementation secondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
